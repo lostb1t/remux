1e9e44b2e4c6989d3ae997e9b1302beb00d544c2
lostb1t <coding-mosses0z@icloud.com>
2026-02-25 14:10:24 +0100

    wip

--- src/jellyfin/models.rs
+++ src/jellyfin/models.rs
@@ -377,7 +377,7 @@ pub struct GetItemsQuery {
     pub studio_ids: Option<Vec<String>>,
     pub exclude_artist_ids: Option<Vec<String>>,
     pub ids: Option<Vec<Uuid>>,
-    pub recursive: bool,
+    pub recursive: Option<bool>,
 }
 
 impl GetItemsQuery {
