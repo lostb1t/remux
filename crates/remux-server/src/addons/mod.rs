7f964916e4881148d66bb28ac39bee251b3caf32
lostbit <coding-mosses0z@icloud.com>
2026-09-16 12:11:27 +0200

    fix: avoid backtrace spam in addon meta fetch error log

--- crates/remux-server/src/addons/mod.rs
+++ crates/remux-server/src/addons/mod.rs
@@ -1741,7 +1741,7 @@ impl AddonService {
                 }
                 Ok(None) => {}
                 Err(e) => {
-                    error!(addon = %r.row.name, error = ?e, "meta addon error")
+                    error!(addon = %r.row.name, error = %e, "meta addon error")
                 }
             }
         }
